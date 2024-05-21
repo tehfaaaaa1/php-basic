29ca5ea6c3921fae66c39e563af37a676b271df0
TEHFAAAA <fateehganteng@gmail.com>
2024-05-21 13:29:57 +0700

    yes

--- database/connect.php
+++ database/connect.php
@@ -8,10 +8,10 @@ $db = 'db_guru';
 $connect = new mysqli($hostname, $user, $pass, $db) or die('Koneksi Database Gagal');
 
 // SQL Shenanigan
-// $sql2 = 'SELECT * FROM `db_guru`.`guru` LIMIT 1000;';
-// $query = $connect->query($sql2);
-// if ($query->num_rows != 0) {
-//     while ($row = $query->fetch_assoc()) {
-//         echo $row['nama'] . '<br>';
-//     }
-// }
+$sql2 = 'SELECT * FROM `db_guru`.`guru` LIMIT 1000;';
+$query = $connect->query($sql2);
+if ($query->num_rows != 0) {
+    while ($row = $query->fetch_assoc()) {
+        echo $row['nama'] . '<br>';
+    }
+}
---
+++ database/update.php
@@ -0,0 +1,11 @@
+<?php
+require_once ('connect.php');
+
+$sql = 'UPDATE db_guru.guru SET nama="DADU" WHERE id=1;';
+
+if ($connect->query($sql) === TRUE) {
+    // echo 'New record created successfully!';
+    echo '<script>alert("Data berhasil diedit!"); location = "connect.php"</script>';
+} else {
+    echo 'Error : ' . $sql . '<br>' . $connect->error;
+}
